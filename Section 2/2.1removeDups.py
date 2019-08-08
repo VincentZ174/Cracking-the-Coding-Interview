817da06c5bd45d9f002a60acf369d62449ec6d0e
Vincent Zheng <VincentZheng174@gmail.com>
2019-08-08 01:05:46 -0400

    Update 2.1removeDups.py

--- Section 2/2.1removeDups.py
+++ Section 2/2.1removeDups.py
@@ -1,65 +1,27 @@
+from linkedlist import LinkedList
+
 hash_table = dict()
-class Node:
-	#constructor
-	def __init__(self, val):
-		self.val = val # set node value
-		self.next = None #set end of linked list to None
-		if val in hash_table:
-			hash_table[val] = hash_table[val] + 1
+
+def deleteDups(ll):
+	if ll.head is None:
+		return
+
+	node = ll.head
+	hash_table[node.value] = node.value
+
+	while node.next:
+		if node.next.value in hash_table:
+			node.next = node.next.next
 		else:
-			hash_table[val] = 1
-
-	def traverse(self):
-		node = self # start at head
-		while node != None: # while not at end of linked list
-			print node.val 
+			hash_table[node.next.value] = node.next.value
 			node = node.next
+	return ll
 
-	def appendToTail(self, val):
-		end = Node(val)
-		n = self
-		while(n.next != None):
-			n = n.next
-		n.next = end
-
-	def delete(self, val):
-		temp = self
-
-		if(temp is not None):
-			if(temp.val == val):
-				self = temp.next
-				temp = None
-				return
-
-		while(temp is not None):
-			if(temp.val == val):
-				break
-			prev = temp
-			temp = temp.next
-
-		if(temp == None):
-			return
-
-		prev.next = temp.next
-		temp = None
-
-	def deleteDups(self):
-		node = self
-		for key,val in hash_table.items():
-			print val
-			if val > 1:
-				node.delete(key)
-
-linkedList = Node(12)
-linkedList.appendToTail(30)
-linkedList.appendToTail(50)
-linkedList.appendToTail(1000)
-linkedList.appendToTail(99)
-linkedList.appendToTail(99)
-linkedList.appendToTail(50)
+ll = LinkedList()
+ll.add_multiple([1,1,2,2,3,4,5,5])
 
 print "----------Linked List----------"
-linkedList.traverse()
-linkedList.deleteDups()
+ll.traverse()
+deleteDups(ll)
 print "----------Deleting Duplicates----------"
-linkedList.traverse()
+ll.traverse()
