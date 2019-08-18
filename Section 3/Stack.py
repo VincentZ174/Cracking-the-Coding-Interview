3fb6870c34de1ff64dc8e5fb1d2758575a4a24be
VincentZ174 <VincentZheng174@gmail.com>
2019-08-18 15:40:13 -0400

    added __str__ and __len__ functions for 3.3 and 3.4

--- Section 3/Stack.py
+++ Section 3/Stack.py
@@ -1,14 +1,41 @@
 class Stack():
+
 	def __init__(self):
+
 		self.items = []
+
+	def __len__(self):
+
+		size = 0
+		for items in self.items:
+			size += 1
+		return size
+
+	def __str__(self):
+
+		return ' '.join(str(x) for x in self.items)
+
 	def push(self, item):
+
 		self.items.append(item)
+
+	def push_multiple(self, items):
+		for item in items:
+			self.items.append(item)
+
 	def pop(self):
+
 		return self.items.pop()
+
 	def get_stack(self):
+
 		return self.items
+
 	def isEmpty(self):
+
 		return self.items == []
+
 	def peek(self):
-		if not self.items.isEmpty():
+
+		if not self.isEmpty():
 			return self.items[-1]
