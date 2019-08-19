2569a83f342c56cd6f24a65ff04cea38a2033271
VincentZ174 <VincentZheng174@gmail.com>
2019-08-18 21:47:31 -0400

    Added testing

--- Section 3/3.3StackOfPlates.py
+++ Section 3/3.3StackOfPlates.py
@@ -1,7 +1,7 @@
 class StackOfPlates:
 
 	def __init__(self, capacity):
-
+		capacity = capacity - 1
 		self.stacks = []
 		if capacity < 1:
 			raise Exception("Must be greater than one")
@@ -32,3 +32,34 @@ class StackOfPlates:
 				del self.stacks[-1][-1]
 
 		return data
+
+	def popAt(self, stackNum):
+
+		if self.stacks[stackNum][-1] == []:
+			raise Exception("Stack is empty")
+		else:
+			if len(self.stacks[stackNum]) == 1:
+				del self.stacks[stackNum]
+			else:
+				del self.stacks[stackNum][-1]
+
+	def peek(self):
+
+		return self.stacks[-1][-1]
+
+stack = StackOfPlates(5)
+stack.push(5)
+stack.push(4)
+stack.push(3)
+stack.push(2)
+stack.push(1)
+stack.pop()
+stack.push(0)
+stack.push(321)
+stack.popAt(1)
+print stack.peek()
+
+
+
+
+
